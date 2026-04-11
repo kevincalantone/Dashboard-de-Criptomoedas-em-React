c2df9f677ff366378940020bb9b52d50dcd7d01c
Kevin Domingues <127136094+kevincalantone@users.noreply.github.com>
2026-04-11 14:07:43 -0300

    feat: setup routing and main navigation

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,26 @@
 import { useState } from 'react'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import Home from './pages/Home'
+import Favories from './pages/Favorites'
+import CoinDetails from './pages/CoinDetails'
+import Navbar from './components/Navbar'
+import './styles/global.css'
 
 
 function App() {
-  const [count, setCount] = useState(0)
+
 
   return (
-    <>
-     
+      <>
+      <BrowserRouter>
+          {/* A Navbar fica fora das Routes para aparecer em todas as páginas */}
+          <Navbar/>
+          <Routes>
+            <Route path='/' element={<Home/>}/>
+            <Route path='/coin/:id' element={<Datails/>}/>
+            <Route path='/favorites' element={<Favorites/>}/>
+          </Routes>
+      </BrowserRouter>
     </>
   )
 }
