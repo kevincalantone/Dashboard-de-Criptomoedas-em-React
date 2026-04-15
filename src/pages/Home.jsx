2ea89f079a963b1c7f11d12c616f87d59b1921f4
suporte.ti <kevin.calantone@DOMAIN.SIVA.COM>
2026-04-15 11:54:37 -0300

    feat: implementa lógica de favoritar moedas

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,3 +1,4 @@
+import { useState } from "react"
 import CoinCard from "../components/CoinCard"
 import useFetch from "../hooks/useFetch"
 
@@ -7,6 +8,17 @@ function Home(){
        '/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=10&page=1&sparkline=false'
     )
 
+    const [favorites, setFavorites] = useState([])
+
+    function handleFav(id){
+        if (favorites.includes(id)){
+            setFavorites(favorites.filter(fav =>fav !== id))
+        } else {
+            setFavorites([...favorites,id])
+        }
+    }
+
+
     if (loading) return <h1 className="loader">Carregando moedas...</h1>
     if (error) return <h1 className="error">Erro ao buscar dados da API</h1>
 
@@ -15,7 +27,12 @@ function Home(){
             <h1 className="title">Mercado de Cripto</h1>
             <div className="coin-grid">
                 {data && data.map(coin =>(
-                  <CoinCard key={coin.id} coin={coin} />
+                  <CoinCard 
+                   key={coin.id}
+                   coin={coin}
+                   isFav={favorites.includes(coin.id)}
+                   onFav={() => handleFav(coin.id)} 
+                   />
                 ))}
             </div>
         </main>
