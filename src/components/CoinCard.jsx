5d1482bd85dd03290c41b32c4e869b18a4b87a0f
suporte.ti <kevin.calantone@DOMAIN.SIVA.COM>
2026-04-16 14:52:07 -0300

    feat: integra lógica de favoritos ao componente CoinCard

--- src/components/CoinCard.jsx
+++ src/components/CoinCard.jsx
@@ -2,15 +2,21 @@
 
 import React from 'react'
 
-const CoinCard = ({coin}) => {
+const CoinCard = ({coin, isFav, onFav}) => {
   return (
    <div className="coin-card">
     <img src={coin.image} alt={coin.name} className='coin-image' />
+   
     <div className="coin-info">
       <h3 className='coin-name'>{coin.name}</h3>
       <p className='coin-price'> U$ {coin.current_price.toLocaleString()}</p>
     </div>
-    <button className='fav-button'>⭐</button>
+   
+    <button className='fav-button'
+      onClick={onFav}
+      style={{filter: isFav ? 'grayscale(0)' : 'grayscale(1)', cursor: 'pointer'}}
+    >⭐
+    </button>
    </div>
   )
 }
