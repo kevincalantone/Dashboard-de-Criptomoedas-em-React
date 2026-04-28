0be1bd3b44dc43e335826460ae3cae473a4ae5f7
Kevin Domingues <127136094+kevincalantone@users.noreply.github.com>
2026-04-27 23:22:05 -0300

    feat: tornar CoinCard clicável e integrar currencyFormatter

--- src/components/CoinCard.jsx
+++ src/components/CoinCard.jsx
@@ -1,23 +1,32 @@
-// A linha ou card com as informações da moeda.
-
 import React from 'react'
+import { Link } from 'react-router-dom' // Importante para a navegação
+import { currencyFormatter } from '../utils/formatter' // Usando seu formatador
 
-const CoinCard = ({coin, isFav, onFav}) => {
+const CoinCard = ({ coin, isFav, onFav }) => {
   return (
-   <div className="coin-card">
-    <img src={coin.image} alt={coin.name} className='coin-image' />
-   
-    <div className="coin-info">
-      <h3 className='coin-name'>{coin.name}</h3>
-      <p className='coin-price'> U$ {coin.current_price.toLocaleString()}</p>
+    <div className="coin-card">
+      {/* Envolvemos a imagem e o nome com um Link para a página de detalhes */}
+      <Link to={`/coin/${coin.id}`} className="coin-link-wrapper">
+        <img src={coin.image} alt={coin.name} className='coin-image' />
+        
+        <div className="coin-info">
+          <h3 className='coin-name'>{coin.name}</h3>
+       
+          <p className='coin-price'>{currencyFormatter(coin.current_price)}</p>
+        </div>
+      </Link>
+      
+      <button 
+        className='fav-button'
+        onClick={(e) => {
+          e.preventDefault(); // Evita que o clique no botão abra a página de detalhes
+          onFav();
+        }}
+        style={{ filter: isFav ? 'grayscale(0)' : 'grayscale(1)', cursor: 'pointer' }}
+      >
+        ⭐
+      </button>
     </div>
-   
-    <button className='fav-button'
-      onClick={onFav}
-      style={{filter: isFav ? 'grayscale(0)' : 'grayscale(1)', cursor: 'pointer'}}
-    >⭐
-    </button>
-   </div>
   )
 }
 
